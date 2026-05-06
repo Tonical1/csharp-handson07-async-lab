333c751fbca2216efb5a291b3093605eaf513a79
Laboratório FIAP <errodrigues2006@outlook.com.br>
2026-05-06 11:27:13 -0300

    Adicionando processor para rodar listas asincronas

---
+++ AsyncLab/Processor.cs
@@ -0,0 +1,37 @@
+﻿public class Processor<TInput, TResult>
+{
+    private readonly IEnumerable<TInput> items;
+
+    public Processor(IEnumerable<TInput> items)
+    {
+        this.items = items;
+    }
+
+    public async Task<List<TResult>> ProcessListAsync(
+        Func<TInput, Task<TResult>> asyncFunc,
+        int maxActive = 4)
+    {
+        var semaphore = new SemaphoreSlim(maxActive);
+        var tasks = new List<Task<TResult>>();
+
+        foreach (var item in items)
+        {
+            await semaphore.WaitAsync();
+
+            tasks.Add(Task.Run(async () =>
+            {
+                try
+                {
+                    return await asyncFunc(item);
+                }
+                finally
+                {
+                    semaphore.Release();
+                }
+            }));
+        }
+
+        var results = await Task.WhenAll(tasks);
+        return results.ToList();
+    }
+}
---
+++ AsyncLab/Queue.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class AsyncDictionaryProcessor<TKey, TValue, TResult>
+{
+    private readonly Dictionary<TKey, TValue> _dict;
+
+    public AsyncDictionaryProcessor(Dictionary<TKey, TValue> dict)
+    {
+        _dict = dict;
+    }
+
+    public async Task<Dictionary<TKey, TResult>> ProcessAsync(
+        Func<TKey, TValue, Task<TResult>> asyncFunc,
+        int maxDegreeOfParallelism = 4)
+    {
+        var semaphore = new SemaphoreSlim(maxDegreeOfParallelism);
+        var tasks = new List<Task>();
+        var results = new Dictionary<TKey, TResult>();
+        var lockObj = new object();
+
+        foreach (var kvp in _dict)
+        {
+            await semaphore.WaitAsync();
+
+            tasks.Add(Task.Run(async () =>
+            {
+                try
+                {
+                    var result = await asyncFunc(kvp.Key, kvp.Value);
+                    lock (lockObj)
+                    {
+                        results[kvp.Key] = result;
+                    }
+                }
+                finally
+                {
+                    semaphore.Release();
+                }
+            }));
+        }
+
+        await Task.WhenAll(tasks);
+        return results;
+    }
+}
